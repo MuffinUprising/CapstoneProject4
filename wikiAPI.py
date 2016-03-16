7d6bc51a2d87ec60370608cfce6ed2b6b893190b
robski577 <robski57@yahoo.com>
2016-03-16 16:34:58 -0500

    adding my wiki api

---
+++ wikiAPI.py
@@ -0,0 +1,33 @@
+import wikipedia
+print("Welcome to Robski Wiki-API")
+user = input("Please enter research item: ")
+
+
+
+mn = wikipedia.search
+print (wikipedia.search(user))
+
+mn = (wikipedia.page(user))
+print (wikipedia.page(user))
+
+
+mn.title
+print ("Title: " + mn.title)
+
+mn.url
+print("URL: " + mn.url)
+
+mn.content
+#print(mn.content)
+imimages
+mn.prop = images|imageinfo
+print(prop)
+
+mn.images[1]
+print(mn.images)
+
+mn.links[3]
+print ("Link: " + mn.links[3])
+
+wikipedia.set_lang("fr")
+wikipedia.summary("Facebook", sentences=1)
